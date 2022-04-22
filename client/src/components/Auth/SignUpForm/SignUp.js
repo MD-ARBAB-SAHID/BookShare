a9e151071e4723b906bfcaea58b37bc2006af5b9
kanheyalal <lalakanheyalalr@gmail.com>
2022-04-23 02:28:36 +0530

    sign up

--- client/src/components/Auth/SignUpForm/SignUp.js
+++ client/src/components/Auth/SignUpForm/SignUp.js
@@ -1,14 +1,23 @@
 import styles from "./SignUp.module.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import SignUpWithEmail from "./SignUpWithEmail/SignUpWithEmail";
 import SignUpWithPhone from "../SignInForm/SignInWithPhone/SignInWithPhone";
-import { FcGoogle } from "react-icons/fc";
 import { FaMobileAlt } from "react-icons/fa";
+import { AiOutlineMail, AiFillGoogleCircle } from "react-icons/ai";
+import AuthContext from "../../../store/auth-context";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../../../firebase-config";
 
 const SignUp = () => {
   const [emailSignIn, setEmailSignIn] = useState(true);
   const [phoneSignIn, setEPhoneSignIn] = useState(false);
   const [googleSignIn, setGoogleSignIn] = useState(false);
+  const provider = new GoogleAuthProvider();
+  const [isLoading, setIsLoading] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   const useEmailSignIn = () => {
     setEmailSignIn(true);
@@ -20,10 +29,19 @@ const SignUp = () => {
     setEPhoneSignIn(true);
     setGoogleSignIn(false);
   };
-  const useGoogleSignIn = () => {
-    setEmailSignIn(false);
-    setEPhoneSignIn(false);
-    setGoogleSignIn(true);
+  const useGoogleSignIn = async () => {
+    try {
+      setIsLoading(true);
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      console.log(credential)
+      const token = credential.accessToken;
+      authCtx.login(credential.idToken, credential.accessToken);
+      setIsLoading(false);
+    } catch (error) {
+        console.log(error.message);
+    }
+    console.log("first")
   };
   return (
     <div className={styles.container}>
@@ -33,16 +51,15 @@ const SignUp = () => {
         {phoneSignIn && <SignUpWithPhone />}
 
         <div className={styles.iconDiv}>
-          {!googleSignIn && (
-            <button onClick={useGoogleSignIn}>
-              <FcGoogle />
-            </button>
-          )}
-          {!phoneSignIn && (
-            <button onClick={usePhoneSignIn}>
-              <FaMobileAlt />
-            </button>
-          )}
+          <button onClick={useEmailSignIn}>
+            <AiOutlineMail />
+          </button>
+          <button onClick={usePhoneSignIn}>
+            <FaMobileAlt />
+          </button>
+          <button onClick={useGoogleSignIn}>
+            <AiFillGoogleCircle />
+          </button>
         </div>
       </div>
     </div>
